aedba186a0a6ea061059fbad8ac3c1205897d9d9
mecolela <mecolela@gmail.com>
2020-04-18 12:51:14 +0300

    Major: Add support for custom snapshot handlers - Fix issues regarding unexpected outcomes when realtime is enabled & configs are given from an upstream node

--- src/Read/Read.js
+++ src/Read/Read.js
@@ -2,39 +2,41 @@ const FirestoreReadNode = require('./ReadNode');
 
 function validateNodeConfig(n) {
   if (!n.collection) {
-    throw "No collection specified";
+    throw 'No collection specified';
   }
 
   if (!n.admin) {
-    throw "No admin specified";
+    throw 'No admin specified';
   }
 }
 
 module.exports = function (RED) {
-  "use strict";
+  'use strict';
 
   function FirestoreRead(n) {
-    validateNodeConfig(n)
+    validateNodeConfig(n);
 
     RED.nodes.createNode(this, n);
     var node = this;
 
+    node.RED = {util: RED.util};
     node.collection = n.collection;
     node.group = n.group;
     node.document = n.document;
     node.realtime = n.realtime;
     node.query = n.query;
+    node.snapHandler = n.snapHandler;
     node.admin = RED.nodes.getNode(n.admin);
 
-    const firestoreReadNode = new FirestoreReadNode(node)
+    const firestoreReadNode = new FirestoreReadNode(node);
     node.on('input', msg => {
-      firestoreReadNode.main(msg, node.send.bind(node), node.error.bind(node))
-    })
+      firestoreReadNode.main(msg, node.send.bind(node), node.error.bind(node));
+    });
 
-    node.on('close', firestoreReadNode.onClose.bind(firestoreReadNode))
+    node.on('close', firestoreReadNode.onClose.bind(firestoreReadNode));
   }
 
-  RED.nodes.registerType("Firestore in", FirestoreRead);
-}
+  RED.nodes.registerType('Firestore in', FirestoreRead);
+};
 
 
--- src/Read/ReadNode.js
+++ src/Read/ReadNode.js
@@ -1,133 +1,196 @@
-const {objectTypeOf} = require('../utils')
+const {objectTypeOf} = require('../utils');
+const util = require('util');
+const vm = require('vm');
 
 function FirestoreReadNode(config) {
   if (!config.admin) {
-    throw 'No firebase admin specified'
+    throw 'No firebase admin specified';
   }
 
   if (!config.collection) {
-    throw 'FireStore collection Not Present'
+    throw 'FireStore collection Not Present';
   }
 
-  this.instance = config.admin.app
-  this.firebase = config.admin.firebase
-  this.firestore = config.admin.firestore
-  this.collection = config.collection
-  this.group = config.group
-  this.document = config.document
-  this.realtime = config.realtime
-  this.query = config.query
-  this.snapListener = null
-  // register a realtime listener on node init
-  if (this.realtime) this.main({}, config.send.bind(config), config.error.bind(config))
+  this.id = config.id;
+  this.RED = config.RED;
+  this.context = config.context().flow;
+  this.instance = config.admin.app;
+  this.firebase = config.admin.firebase;
+  this.firestore = config.admin.firestore;
+  this.collection = config.collection;
+  this.group = config.group;
+  this.document = config.document;
+  this.realtime = config.realtime;
+  this.query = config.query;
+  this.snapHandler = config.snapHandler;
+  this.snapListener = null;
+
+  // register a realtime listener on node start
+  if (this.realtime) {
+    const msg = {firestore: {}};
+    const res = this.context.get(`$fst-input-${this.id}`);
+    msg.firestore = res || {};
+    this.main(msg, config.send.bind(config), config.error.bind(config));
+  }
 }
 
-FirestoreReadNode.prototype.main = function (msg, send, errorCb) {
-  const input = (msg.hasOwnProperty('firestore')) ? msg.firestore : {}
-  msg.firebase = {app: this.instance, admin: this.firebase}
+FirestoreReadNode.prototype.main = function (msg, send, error) {
+  const input = (msg.hasOwnProperty('firestore')) ? msg.firestore || {} : {};
+  msg.firebase = {app: this.instance, admin: this.firebase};
 
-  const col = input.collection || this.collection
-  const group = input.group || this.group
-  const doc = input.document || this.document
-  const rt = input.realtime || this.realtime
-  const query = input.query || this.query
-  const disable = input.disableHandler || false
+  const col = input.collection = input.collection || this.collection;
+  const group = input.group = input.group || this.group;
+  const doc = input.document = input.document || this.document;
+  const rt = input.realtime = input.realtime || this.realtime;
+  const query = input.query = input.query || this.query;
+  const disable = input.disableHandler = input.disableHandler || false;
 
-  if (doc && group) throw 'Cannot set document ref in a collection group query'
+  // Resolves any defaults across the entire node
+  Object.keys(input).forEach(key => this[key] = input[key]);
 
-  let baseRef, referenceQuery
+  let snapHandler = this.snapHandler;
+  if (!disable) {
+    snapHandler = this.prepareSnapHandler(this.snapHandler, {
+      config: input,
+      context: this.context,
+      RED: this.RED, msg
+    });
+  }
+
+  if (rt) {
+    // Save input to storage to better handle realtime recalls on node restarts
+    try {
+      this.context.set(`$fst-input-${this.id}`, input);
+    } catch (e) {
+    }
+  }
+
+  if (doc && group) throw 'Cannot set document ref in a collection group query';
+
+  let baseRef, referenceQuery;
   if (group) {
-    baseRef = this.firestore.collectionGroup(col)
-    referenceQuery = this.prepareQuery(baseRef, query)
+    baseRef = this.firestore.collectionGroup(col);
+    referenceQuery = this.prepareQuery(baseRef, query);
   } else {
-    baseRef = doc ? this.firestore.collection(col).doc(doc) : this.firestore.collection(col)
-    referenceQuery = doc ? baseRef : this.prepareQuery(baseRef, query)
+    baseRef = doc ? this.firestore.collection(col).doc(doc) : this.firestore.collection(col);
+    referenceQuery = doc ? baseRef : this.prepareQuery(baseRef, query);
   }
 
   // remove existing listener before registering another
-  this.unsubscribeListener()
+  this.unsubscribeListener();
 
   if (disable) {
-    msg.payload = referenceQuery
-    return send(msg)
+    msg.payload = referenceQuery;
+    return send(msg);
   }
 
   if (!rt) {
     referenceQuery.get()
       .then((snap) => snapHandler(snap))
-      .catch((err) => errorCb(err, msg))
-  } else {
-    this.snapListener = referenceQuery.onSnapshot((snap) => snapHandler(snap), (error) => errorCb(error, msg))
-  }
-
-  function snapHandler(snap) {
-    if (!doc) { // get an entire collection
-      let docArray = {}
-      snap.forEach(function (snapDoc) {
-        if (!snapDoc.exists) return
-        docArray[snapDoc.id] = snapDoc.data()
+      .then((result) => {
+        msg.payload = result;
+        send(msg);
       })
-      msg.payload = docArray
-    } else {
-      msg.payload = snap.data()
-    }
-    send(msg)
+      .catch((err) => error(err, msg));
+  } else {
+    this.snapListener = referenceQuery.onSnapshot((snap) => {
+      Promise.resolve(snapHandler(snap))
+        .then((result) => {
+          msg.payload = result;
+          send(msg);
+        }).catch(err => error(err, msg));
+    }, (err) => error(err, msg));
   }
-}
+};
 
 FirestoreReadNode.prototype.prepareQuery = function (baseRef, queryObj) {
   queryObj.forEach((condition) => {
-    const key = Object.keys(condition)[0]
+    const key = Object.keys(condition)[0];
 
-    let value = condition[key]
-    let isString = objectTypeOf(value) === '[object String]'
-    let isArray = objectTypeOf(value) === '[object Array]'
-    let isObject = objectTypeOf(value) === '[object Object]'
+    let value = condition[key];
+    let isString = objectTypeOf(value) === '[object String]';
+    let isArray = objectTypeOf(value) === '[object Array]';
+    let isObject = objectTypeOf(value) === '[object Object]';
 
     switch (key) {
       case 'where':
         if (isString) {
-          baseRef = baseRef.where(value)
+          baseRef = baseRef.where(value);
         } else if (isArray) {
-          baseRef = baseRef.where(...value)
+          baseRef = baseRef.where(...value);
         } else if (isObject) {
-          baseRef = baseRef.where(value.field, value.operation, value.value)
+          baseRef = baseRef.where(value.field, value.operation, value.value);
         }
-        break
+        break;
       case 'orderBy':
         if (isString) {
-          baseRef = baseRef.orderBy(value)
+          baseRef = baseRef.orderBy(value);
         } else if (isArray) {
-          baseRef = baseRef.orderBy(...value)
+          baseRef = baseRef.orderBy(...value);
         } else if (isObject) {
           baseRef = !!value.direction ?
             baseRef.orderBy(value.field, value.direction)
-            : baseRef.orderBy(value.field)
+            : baseRef.orderBy(value.field);
         }
-        break
+        break;
       case 'startAt':
       case 'endAt':
       case 'startAfter':
       case 'endBefore':
-        baseRef = isArray ? baseRef[key](...value) : baseRef[key](value)
-        break
+        baseRef = isArray ? baseRef[key](...value) : baseRef[key](value);
+        break;
       case 'limit':
       case 'offset':
-        baseRef = baseRef[key](value)
-        break
+        baseRef = baseRef[key](value);
+        break;
     }
-  })
+  });
 
-  return baseRef
-}
+  return baseRef;
+};
+
+FirestoreReadNode.prototype.prepareSnapHandler = function (func, ctx = {}) {
+  if (!func) return this.defaultSnapHandler.bind(this);
+  const sandbox = {
+    ...ctx,
+    util: util,
+    Date: Date,
+    Buffer: Buffer,
+    Promise: Promise,
+    console: console
+  };
+  const context = vm.createContext(sandbox);
+  const script = new vm.Script(`(function() {return function(snap) {${func}}})()`, {
+    filename: this.id + (this.name ? '[' + this.name + ']' : '') + 'custom handler',
+    displayErrors: true
+  });
+  try {
+    return script.runInContext(context);
+  } catch (e) {
+    throw e.message || e;
+  }
+};
+
+FirestoreReadNode.prototype.defaultSnapHandler = function (snap) {
+  if (!this.document) { // get an entire collection
+    const docArray = {};
+    snap.forEach(function (snapDoc) {
+      if (!snapDoc.exists) return;
+      docArray[snapDoc.id] = snapDoc.data();
+    });
+    return docArray;
+  } else {
+    return snap.data();
+  }
+};
 
 FirestoreReadNode.prototype.unsubscribeListener = function () {
-  if (objectTypeOf(this.snapListener) === '[object Function]') this.snapListener()
-}
+  if (objectTypeOf(this.snapListener) === '[object Function]') this.snapListener();
+};
 
 FirestoreReadNode.prototype.onClose = function (done) {
-  this.unsubscribeListener()
-  done()
-}
+  this.unsubscribeListener();
+  done();
+};
 
-module.exports = FirestoreReadNode
+module.exports = FirestoreReadNode;
