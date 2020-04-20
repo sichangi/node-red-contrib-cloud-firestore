775ae7070e7cc7714939125108dd847c3d333eb7
Jean-Baptiste Richardet <jb@cameo-talents.fr>
2020-04-20 22:36:29 +0200

    Show status of node
    * Adds 3 status states: Querying, Success and Error
    * `main` function always return `Promises`
    * Send a `done` signal on finish, to comply with NodeRED 1.0.0 guidelines

--- src/Read/Read.js
+++ src/Read/Read.js
@@ -28,10 +28,21 @@ module.exports = function (RED) {
     node.eject = n.eject;
     node.snapHandler = n.snapHandler;
     node.admin = RED.nodes.getNode(n.admin);
+    node.status({});
 
     const firestoreReadNode = new FirestoreReadNode(node);
-    node.on('input', msg => {
-      firestoreReadNode.main(msg, node.send.bind(node), node.error.bind(node));
+    node.on('input', (msg, send, done) => {
+      node.status({fill:"blue",shape:"ring",text:"querying..."})
+      firestoreReadNode.main(msg, node.send.bind(node), node.error.bind(node))
+      .then(() => {
+        node.status({fill:"green",shape:"dot",text:"success"})
+        // Tells NodeRED we've finished
+        if(done) done()
+      })
+      .catch((err) => {
+        node.status({fill:"red",shape:"dot",text:"error"})
+        node.error(err)
+      })
     });
 
     node.on('close', firestoreReadNode.onClose.bind(firestoreReadNode));
